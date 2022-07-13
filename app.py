58cf4d70f9e3b6630e2611286026a14d853b08f9
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-07-13 23:54:29 +0900

    test

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, render_template
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Activation, ZeroPadding2D
+from deepface import DeepFace
+import numpy as np
+import os
+
+os.getcwd()
+
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(Conv2D(4096, (7, 7), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(4096, (1, 1), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(2622, (1, 1)))
+model.add(Flatten())
+model.add(Activation('softmax'))
+
+model.add(Dense(128, activation='relu'))
+model.add(Dense(5))
+model.add(Activation('softmax'))
+model.load_weights('./missing_model.h5')
+
+f = open("/Users/chance/Desktop/missing/missing-flask/static/name_list.csv", 'r')
+lines = f.readlines()
+li = []
+for line in lines:
+    li.append(line.strip())
+f.close()
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def predict():
+    img = DeepFace.detectFace('/Users/chance/Desktop/missing/missing-flask/static/임혜영0.jpeg')
+    img = img[np.newaxis, :]
+    score = model.predict(img)
+    high_score = np.argmax(score)
+    return render_template("index.html", data=li[high_score])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2000)
